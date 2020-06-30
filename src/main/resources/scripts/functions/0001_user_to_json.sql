16ed1efd40a3198b0ae78b9dc4097df848ddaf13
JordanGibson <jordang@kainos.com>
2020-06-30 14:23:12 +0100

    complete v1

--- src/main/resources/scripts/functions/0001_user_to_json.sql
+++ src/main/resources/scripts/functions/0001_user_to_json.sql
@@ -1,13 +1,9 @@
-CREATE OR REPLACE FUNCTION user_to_json (name varchar, role_name varchar, street varchar, town varchar) RETURNS jsonb AS $$
+CREATE OR REPLACE FUNCTION user_to_json (name varchar, role_name varchar) RETURNS jsonb AS $$
 	BEGIN
  	RETURN json_build_object(
  			'name', name,
 			'role', json_build_object(
 				'name', role_name
-			),
-			'address', json_build_object(
-				'street', street,
-				'town', town
 			)
  		);
 	END;
--- src/main/resources/scripts/views/0002_user_json.sql
+++ src/main/resources/scripts/views/0002_user_json.sql
@@ -1,5 +1,7 @@
 SELECT
 	user_to_json(
 		u.first_name || ' ' || u.last_name,
-		' ')
-FROM user u;
+		r.name)
+FROM "user" u
+JOIN "role" r
+	ON r.id = u.role_id;
