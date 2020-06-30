62d0a8a52d70aaf88c0a199b7b0fbfa8d5368e3f
JordanGibson <jordang@kainos.com>
2020-06-30 14:36:00 +0100

    complete v2 changes

--- src/main/resources/scripts/views/0002_user_json.sql
+++ src/main/resources/scripts/views/0002_user_json.sql
@@ -1,7 +1,11 @@
 SELECT
 	user_to_json(
 		u.first_name || ' ' || u.last_name,
-		r.name)
+		r.name,
+		a.street,
+		a.town)
 FROM "user" u
 JOIN "role" r
-	ON r.id = u.role_id;
+	ON r.id = u.role_id
+LEFT JOIN "address" a
+	ON a.id = u.address_id;
